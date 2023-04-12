ee531193a717c2573754a265d0a625cf88b9bfe4
FaixFarisky <130310605+FaixFarisky@users.noreply.github.com>
2023-04-12 15:45:12 +0700

    transaksi backend
    menambahkan router ke index

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 5000;
 // routers
 const routerAkunToko = require('./routes/akuntoko');
 const routerStokBarang = require('./routes/stokBarang');
+const routertransaksi = require('./routes/transaksi');
 
 // Untuk menerima req.body
 app.use(express.json());
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended:true }));
 // call router
 app.use(routerAkunToko);
 app.use(routerStokBarang);
+app.use(routertransaksi);
 
 app.listen(port,() => {
     console.log(`Server Berjalan Pada Port ${port}`);
