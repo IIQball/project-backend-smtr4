21ed972a770eb3e9dace5795cfc061e4b2ac90f4
IIQball <muhammadiqbl45@gmail.com>
2023-06-25 00:44:56 +0700

    hubungkan dengan front end page stock barang
    atur koneksi dengan front end page stock barang untuk display semua stock

--- controller/stokBarang.js
+++ controller/stokBarang.js
@@ -1,8 +1,15 @@
 const connection = require('../db/db.js');
 
 module.exports = {
-    getStokBarang : (req,res)=>{
-        const dbquery = "SELECT * FROM stok_barang";
+    getAllStock : (req,res)=>{
+        const idUser = req.params.idUser;
+        const dbquery = `
+        select akun_toko.namaToko, stok_barang.kodeBarang, data_barang.namaBarang,
+        data_barang.ukuran, data_barang.harga, stok_barang.stok
+        from stok_barang
+        inner join akun_toko on stok_barang.idToko = akun_toko.idToko
+        inner join data_barang on stok_barang.kodeBarang = data_barang.kodeBarang
+        where stok_barang.idUser = ${idUser};`;
         connection.query(dbquery,(err,data)=>{
             if (err){
                 console.log("error : ", err);
@@ -13,74 +20,4 @@ module.exports = {
             else res.send(data);
         });
     },
-    getStokToko : (req,res)=>{
-        const dbquery = "SELECT * FROM stok_barang";
-        connection.query(dbquery,(err,data)=>{
-            if (err){
-                console.log("error : ", err);
-                res.status(500).send({
-                    message : err.message || "Terjadi error saat GET data"
-                });
-            }
-            else res.send(data);
-        });
-    },
-    getByID : (req,res)=>{
-        const idProduk = req.params.id;
-        const dbquery = `SELECT * FROM stok_barang WHERE id_produk = '${idProduk}'`;
-        connection.query(dbquery,(err,data)=>{
-            if (err){
-                console.log("error : ", err);
-                res.status(500).send({
-                    message : err.message || `Terjadi error saat GET data barang dengan id '${idProduk}'`
-                })
-            }
-            else res.send(data);
-        });
-    },
-    inputData : (req,res)=>{
-        const dataBarang = req.body;
-        const dbquery = "INSERT INTO stok_barang SET ?";
-        connection.query(dbquery,dataBarang,(err)=>{
-            if (err){
-                console.log("error : ", err);
-                res.status(500).send({
-                    message : err.message || "Terjadi error saat input data"
-                })
-            }
-            else res.send(dataBarang);
-        });
-    },
-    editData : (req,res)=>{
-        const idProduk = req.params.id;
-        const dataBarang = req.body;
-        const dbquery = `UPDATE stok_barang SET nama_barang = '${dataBarang.nama_barang}', kategori = '${dataBarang.kategori}', harga = '${dataBarang.harga}', jumlah = '${dataBarang.jumlah}' WHERE id_produk = '${idProduk}'`;
-        connection.query(dbquery,(err)=>{
-            if(err){
-                console.log("error : ", err);
-                res.status(500).send({
-                    message : err.message || `Terjadi error saat update data barang dengan id '${idProduk}'`
-                })
-            }
-            else{
-                console.log("update data barang : ", {id_produk: idProduk, ...dataBarang});
-                res.send({id_produk: idProduk, ...dataBarang});
-            }
-        })
-    },
-    delData : (req,res)=>{
-        const idProduk = req.params.id;
-        const dbquery = `DELETE FROM stok_barang WHERE id_produk = '${idProduk}'`
-        connection.query(dbquery,(err)=>{
-            if(err){
-                console.log("error : ",err);
-                res.status(500).send({
-                    message : err.message || `Terjadi error saat ingin menghapus data dengan id '${idProduk}'`
-                })
-            }
-            else{
-                res.send(`Berhasil Menghapus data dengan id '${idProduk}' dari Database`);
-            }
-        });
-    }
 }
