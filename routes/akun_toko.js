5077b1efbc2d755e0401080a6eb3393cc6e51e75
Rifqi007 <130329176+Rifqi007@users.noreply.github.com>
2023-07-10 11:33:28 +0700

    Revisi-BackEnd

--- routes/akun_toko.js
+++ routes/akun_toko.js
@@ -2,7 +2,7 @@ const express = require('express')
 const routerToko = express.Router()
 const ctrlToko = require('../controller/akun_toko')
 
-routerToko.get('/toko',ctrlToko.getToko)
+routerToko.get('/:idUser/toko',ctrlToko.getToko)
 routerToko.get('/toko/:idToko',ctrlToko.getTokoById)
 routerToko.post('/register',ctrlToko.register)
 routerToko.put('/toko/:id', ctrlToko.editToko)
